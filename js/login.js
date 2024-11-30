00f1668e766eb5422132a910cac546131dfc65b1
momin096 <momin09609@gamil.com>
2024-12-01 02:34:14 +0600

    home structure added

--- js/login.js
+++ js/login.js
@@ -2,16 +2,17 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
     
-
-
     // get the phone number
-    const phoneNumber = document.getElementById('phone');
-    const phone = phoneNumber.value ;
-
+    const phoneNumber = document.getElementById('phone').value;
     // get the pin
-    const pinNumber = document.getElementById('pin');
-    const pin = pinNumber.value ; 
-
-    console.log(phone, pin)
+    const pinNumber = document.getElementById('pin').value; 
+    // check phone and pin
+    if(phoneNumber === '01611175626' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = './home.html';
+    }
+    else{
+        alert('Wrong Phone number or Pin');
+    }
 
 })
