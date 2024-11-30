afcf86122a324ec2007c95ecd1f40feb82a50828
momin096 <momin09609@gamil.com>
2024-12-01 03:13:49 +0600

    add money feature added

--- js/home.js
+++ js/home.js
@@ -1,2 +1,19 @@
+// Add money to the account
+document.getElementById('add-money').addEventListener('click', function (event) {
+    event.preventDefault(); // prevent page reload after form submit
+    const addMoneyInput = document.getElementById('add-amount').value;
+    const addMoney = parseInt(addMoneyInput);
+    const pinNumberINput = document.getElementById('add-pin').value;
 
-document.getElementById('')
+    if (pinNumberINput === '1234') {
+        // get the current balance
+        const balanceConst = document.getElementById('balance').innerText;
+        const balance = parseInt(balanceConst);
+        const newBalance = balance + addMoney;
+
+        document.getElementById('balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add Money ! Please Try again');
+    }
+})
